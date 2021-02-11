262030667ed13be08e93d68040aa9680c226279e
Hersain Lozada Hermosa <hersainlozada@gmail.com>
2021-02-11 18:53:51 -0500

    Array_02_02_2021

---
+++ main.js
@@ -0,0 +1,21 @@
+let lista1 = ['baby','you'];
+let lista2 = ['hersain','julian','diana','diego', lista1, 'pedro','juan','elías','gabriel','galatiel','samael','kal-el'];
+
+
+// let resultado = lista.pop();
+// let resultado = lista.push(100);
+// let resultado = lista.shift();
+//let resultado = lista.unshift({nombre:"Profesor"});
+// let resultado = lista.splice(5,1,"prueba");
+// let resultado = lista.sort();
+// let resultado = lista.reverse();
+// let resultado = lista.join("-");
+// let resultado = lista.concat(lista2);
+// let resultado = lista.slice(1,4);
+// let resultado = lista.some(data => data=="adrian");
+
+
+console.log(`Lista original`);
+console.log(lista1);
+console.log(`Lista afectada`);
+console.log(lista2);
